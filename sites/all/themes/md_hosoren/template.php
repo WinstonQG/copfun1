b15fadfde4ca2cc3d90aabfdd6a814b406cc774a
alex posidelov <alex.posidelov@gmail.com>
2016-02-05 09:58:38 +0100

    categories fix on related block

--- sites/all/themes/md_hosoren/template.php
+++ sites/all/themes/md_hosoren/template.php
@@ -100,7 +100,9 @@ function md_hosoren_preprocess_node(&$vars) {
     $field_categories = field_get_items('node', $node, 'field_product_category');    
     if (isset($field_categories) && !empty($field_categories)) {
       foreach ($field_categories as $key => $categories_item) {
-        $categories_content .= l($categories_item['taxonomy_term']->name, 'taxonomy/term/' . $categories_item['taxonomy_term']->tid);
+        if (isset($categories_item['taxonomy_term'])) {
+          $categories_content .= l($categories_item['taxonomy_term']->name, 'taxonomy/term/' . $categories_item['taxonomy_term']->tid);
+        }
       }
     }
     $vars['categories_list'] = $categories_content;
