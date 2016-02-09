7570fcded10ffcb1ece310b90c83b74750db26d9
alex posidelov <alex.posidelov@gmail.com>
2016-02-09 18:33:55 +0100

    new categories and views - prepare for menu

--- sites/all/themes/md_hosoren/templates/page/categories-list.tpl.php
+++ sites/all/themes/md_hosoren/templates/page/categories-list.tpl.php
@@ -1,20 +1,23 @@
 <div class="l-categories-list">
 	<div class="container">
 		<div class="row">		
-	    <div class="col-md-15">
+	    <div class="col-md-2">
 	    	<?php print render($page['categories_list_1']); ?>
 	    </div>
-	    <div class="col-md-15">
+	    <div class="col-md-2">
 	    	<?php print render($page['categories_list_2']); ?>
 	    </div>
-	    <div class="col-md-15">
+	    <div class="col-md-2">
 	    	<?php print render($page['categories_list_3']); ?>
 	    </div>
-	    <div class="col-md-15">
+	    <div class="col-md-2">
 	    	<?php print render($page['categories_list_4']); ?>
 	    </div>
-	    <div class="col-md-15">
+	    <div class="col-md-2">
 	    	<?php print render($page['categories_list_5']); ?>
+	    </div>
+	    <div class="col-md-2">
+	    	<?php print render($page['categories_list_6']); ?>
 	    </div>
 		</div>
 	</div>
